4277d50951dbba3729f84def77e817761df4448a
Cédric Belin <cedric@belin.io>
2016-02-28 19:10:12 +0100

    Code formatting

--- bin/cli.js
+++ bin/cli.js
@@ -7,11 +7,11 @@
 'use strict';
 
 // Module dependencies.
-const Application=require('../lib/app');
+const Application = require('../lib/app');
 
 // Run the application.
-if(module===require.main) {
-  process.title='reverse-proxy.js';
+if(module === require.main) {
+  process.title = 'reverse-proxy.js';
   new Application().run();
 }
-else module.exports=Application;
+else module.exports = Application;
