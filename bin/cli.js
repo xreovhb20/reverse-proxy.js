9829bf25c8fa2f386e89b163c8590d73fafb60fb
Cédric Belin <cedric@belin.io>
2017-03-06 22:22:26 +0100

    Fixed a bug

--- bin/cli.js
+++ bin/cli.js
@@ -13,7 +13,7 @@ async function main() {
 }
 
 // Run the application.
-if (module === require.main) main.catch(error => {
+if (module === require.main) main().catch(error => {
   console.log(error.message);
   process.exit(1);
 });
