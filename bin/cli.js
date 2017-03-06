92cc73459eb14807af5368dee3726c7269c790e0
Cédric Belin <cedric@belin.io>
2017-03-06 20:37:00 +0100

    Improved the CLI

--- bin/cli.js
+++ bin/cli.js
@@ -1,6 +1,19 @@
 #!/usr/bin/env node
-process.title = 'Reverse-Proxy.js';
+'use strict';
 
 const {Application} = require('../lib');
-global.app = new Application();
-global.app.run();
+
+/**
+ * Application entry point.
+ * @return {Promise} Completes when the program is started.
+ */
+async function main() {
+  process.title = 'Reverse-Proxy.js';
+  return new Application().run();
+}
+
+// Run the application.
+if (module === require.main) main.catch(error => {
+  console.log(error.message);
+  process.exit(1);
+});
